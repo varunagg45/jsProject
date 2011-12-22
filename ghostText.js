a53babff32643614bb8ed092a1c338f5a926901c
Varun Aggarwal <agg.varun@gmail.com>
2011-12-22 10:08:26 -0800

    added licensing information

--- ghostText.js
+++ ghostText.js
@@ -1,3 +1,35 @@
+/**
+ * Author: Varun Aggarwal
+ * email: agg.varun@gmail.com
+ * 
+ * 
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ * 
+ * 
+ * @param {String}: "txt": The string that would be added as the default text when nothing is provided.
+ * @param {String}: "type": By default it considers the element as input field. But you can pass another parameter "PASSWORD" so that it produces the ghost text for the password field.
+ * 
+ */
 (function( $ ) {
   $.fn.addGhostText = function( txt, type){
   	if( $.trim(txt) == ""){
